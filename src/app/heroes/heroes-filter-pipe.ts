321de4529139cb68d817ed083745139e4fcd5d9a
IEVN1001-23002280 <85003@alumnos.utleon.edu.mx>
2026-09-19 10:28:12 -0600

    Pipes 19-09-2026

---
+++ src/app/heroes/heroes-filter-pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'heroesFilter',
+  standalone: false,
+})
+export class HeroesFilterPipe implements PipeTransform {
+  transform(value:IHeroes[], args: string): IHeroes[]{
+    let filter:string=args?args.toLocaleLowerCase():'';
+
+    return filter?value.filter((heroe:IHeroes)=>
+      heroe.nombre.toLocaleLowerCase().indexOf(filter)!==-1):value;
+  }
+}
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IHeroes} from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
   templateUrl: './heroes-list.html',
 })
 
-export class HeroesList {}
+export class HeroesList {
+imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+  heroes:IHeroes[]=[
+    
+  {
+    imagen:'https://dragonball-api.com/characters/celula.webp',
+    nombre:'CELL',
+    description:'El androide perfecto',
+    race:'Androide',
+    ki:5000000
+    },
+        {
+    imagen:'https://dragonball-api.com/characters/gohan.webp',
+    nombre:'Gohan',
+    description:'El hijo do goki',
+    race:'Saiyan',
+    ki:6000000
+    },
+        {
+    imagen:'https://dragonball-api.com/characters/Krilin_Universo7.webp',
+    nombre:'Krillin',
+    description:'El humano mas fuerte',
+    race:'Androide',
+    ki:800000
+    },
+        {
+    imagen:'https://dragonball-api.com/characters/Gotenks_Artwork.webp',
+    nombre:'Gotenks',
+    description:'La Fusion mas fuerte',
+    race:'Saiyan',
+    ki:8500000
+    }
+    
+  ]
+}
